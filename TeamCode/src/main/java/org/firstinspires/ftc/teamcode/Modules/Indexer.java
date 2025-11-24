ea0530eff18a1e1f09d8037fd915b85832359c92
parthgoyal <epicparth@gmail.com>
2025-11-24 14:34:42 -0500

    Firing Logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Indexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Indexer.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Modules;
 
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -13,6 +15,9 @@ public class Indexer {
     ElapsedTime timer = new ElapsedTime();
     String[] order;
 
+    Servo[] kickers;
+
+
     public Indexer(RevColorSensorV3 slotOne, RevColorSensorV3 slotTwo, RevColorSensorV3 slotThree, Servo kickerOne, Servo kickerTwo, Servo kickerThree, String[] order, ElapsedTime timer){
         this.slotOne = slotOne;
         this.slotTwo = slotTwo;
@@ -21,6 +26,8 @@ public class Indexer {
         this.kickerOne = kickerOne;
         this.kickerTwo = kickerTwo;
         this.kickerThree = kickerThree;
+
+        kickers = new Servo[]{kickerOne, kickerTwo, kickerThree};
 
         this.order = order;
         this.timer = timer;
@@ -35,12 +42,35 @@ public class Indexer {
         return colors;
     }
 
-    public void fire(){
+
+
+    //TODO: Replace sleep waits with resetting timer to preserve Tele-Op Thread
+    public void fire() throws InterruptedException {
         String[] slots = getReadings();
 
-        if(slots[0] == "Empty" || slots[1] == "Empty" || slots[2] == "Empty"){
+        if(slots[0].equals("Empty") || slots[1].equals("Empty") || slots[2].equals("Empty")){
             kickerOne.setPosition(1);
-            timer.reset();
+            sleep(500);
+            kickerOne.setPosition(0);
+            kickerTwo.setPosition(1);
+            sleep(500);
+            kickerTwo.setPosition(0);
+            kickerThree.setPosition(1);
+        }else{
+            boolean[] fired = {false, false, false};
+
+            for(int i = 0; i<order.length; i++){
+                for(int j = 0; j<slots.length; j++){
+                    if(slots[j].equals(order[i]) && !fired[j]){
+                       kickers[j].setPosition(1);
+                       fired[i] = true;
+                       sleep(200);
+                       kickers[j].setPosition(0);
+
+                       break;
+                    }
+                }
+            }
         }
 
     }
